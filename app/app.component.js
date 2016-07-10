22ab80dfead5000527241eb2010c91f6eaffee3d
jesse soldat <jsoldat@hotmail.com>
2016-07-10 08:58:01 +0700

    created a router

--- app/app.component.js
+++ app/app.component.js
@@ -9,8 +9,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
-var heroes_component_1 = require('./heroes.component');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Super Heroes';
@@ -18,8 +18,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: " <h1>{{title}}</h1>\n\t\t\t\t<my-heroes></my-heroes>\n\n\n\t",
-            directives: [heroes_component_1.HeroesComponent],
+            template: "\n\t  <h1>{{title}}</h1>\n\t  <a [routerLink]=\"['/heroes']\">Heroes</a>\n\t  <router-outlet></router-outlet>\n\t",
+            directives: [router_1.ROUTER_DIRECTIVES],
             providers: [hero_service_1.HeroService]
         }), 
         __metadata('design:paramtypes', [])
